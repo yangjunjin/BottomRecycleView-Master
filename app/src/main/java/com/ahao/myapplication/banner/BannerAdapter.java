d40d9f86c8df4fec2286502ac37967b9336db472
yangjunjin <yangjunjin>
2019-08-07 10:57:31 +0800

    ffdfd

---
+++ app/src/main/java/com/ahao/myapplication/banner/BannerAdapter.java
@@ -0,0 +1,108 @@
+package com.ahao.myapplication.banner;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.ahao.myapplication.R;
+
+import java.util.List;
+import java.util.Random;
+
+public class BannerAdapter extends RecyclerView.Adapter<BannerAdapter.BannerViewHolder> {
+
+    private List<String> data;
+
+    private Context context;
+
+    private Random random = new Random();
+
+    private float itemWidth = 1.0f;
+    private float ratio = 0.5f;  // 宽高比
+
+    private DisplayMetrics displayMetrics;
+
+    public BannerAdapter(List<String> data, Context context) {
+        super();
+        this.data = data;
+        this.context = context;
+        displayMetrics = context.getResources().getDisplayMetrics();
+    }
+
+    @NonNull
+    @Override
+    public BannerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.view_banner_item, parent, false);
+        final BannerViewHolder holder = new BannerViewHolder(view);
+        holder.textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "onclick :" + holder.getAdapterPosition(), Toast.LENGTH_SHORT).show();
+                listener.onClick();
+            }
+        });
+        return holder;
+    }
+
+    public onClickListener getListener() {
+        return listener;
+    }
+
+    public BannerAdapter setListener(onClickListener listener) {
+        this.listener = listener;
+        return this;
+    }
+
+    onClickListener listener;
+    public interface onClickListener {
+        void onClick();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final BannerViewHolder holder, final int position) {
+        holder.textView.setText(data.get(position));
+        holder.textView.setBackgroundColor(Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+//        ViewGroup.LayoutParams layoutParams = holder.textView.getLayoutParams();
+//        layoutParams.width = (int) (displayMetrics.widthPixels * itemWidth);
+//        layoutParams.height = (int) (layoutParams.width * ratio);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class BannerViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public BannerViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.text);
+        }
+
+    }
+
+    public float getItemWidth() {
+        return itemWidth;
+    }
+
+    public void setItemWidth(float itemWidth) {
+        this.itemWidth = itemWidth;
+    }
+
+    public float getRatio() {
+        return ratio;
+    }
+
+    public void setRatio(float ratio) {
+        this.ratio = ratio;
+    }
+}
---
+++ app/src/main/java/com/ahao/myapplication/banner/BannerRecyclerActivity.java
@@ -0,0 +1,101 @@
+package com.ahao.myapplication.banner;
+
+import android.animation.ValueAnimator;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.widget.RelativeLayout;
+
+import com.ahao.myapplication.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class BannerRecyclerActivity extends Activity {
+    @BindView(R.id.recycler_view2)
+    RecyclerView recyclerView2;
+    BannerAdapter adapter2;
+
+    private List<String> data = new ArrayList<>();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banner_recycler_view);
+        ButterKnife.bind(this);
+        initData();
+        initDataSource();
+    }
+
+    private void initData() {
+        adapter2 = new BannerAdapter(data, this);
+        BannerLayoutManager layoutManager2 = new BannerLayoutManager();
+        adapter2.setItemWidth(0.8f);//item的宽
+        adapter2.setRatio(0.6f);
+        recyclerView2.setAdapter(adapter2);
+        recyclerView2.setLayoutManager(layoutManager2);
+
+        BannerPageSnapHelper bannerPageSnapHelper2 = new BannerPageSnapHelper();
+        bannerPageSnapHelper2.setInfinite(true);
+        bannerPageSnapHelper2.attachToRecyclerView(recyclerView2);
+
+        adapter2.setListener(new BannerAdapter.onClickListener() {
+            @Override
+            public void onClick() {
+                changeHeight();
+            }
+        });
+    }
+
+    boolean isOpen = false;//是否打开了
+
+    private void changeHeight() {
+//   RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) recyclerView2.getLayoutParams();  
+//                params.width = dip2px(MainActivity.this, width);  
+//                params.height = dip2px(MainActivity.this, height);  
+//                // params.setMargins(dip2px(MainActivity.this, 1), 0, 0, 0); // 可以实现设置位置信息，如居左距离，其它类推  
+//                // params.leftMargin = dip2px(MainActivity.this, 1);  
+//                imageView.setLayoutParams(params);  
+
+        final RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) recyclerView2.getLayoutParams();
+        params.width = params.width;
+        params.height = params.height;
+        float start = params.height, end = 0f;
+        end = isOpen ? dip2px(250) : dip2px(440);
+
+        //执行动画
+        ValueAnimator anim = ValueAnimator.ofFloat(start, end);
+        anim.setDuration(300);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float currentValue = (float) animation.getAnimatedValue();
+                params.height = (int) currentValue;
+                recyclerView2.setLayoutParams(params);
+                recyclerView2.invalidate();
+            }
+        });
+        anim.start();
+        isOpen = !isOpen;
+    }
+
+    private void startAnimation(float start,float end) {
+
+    }
+
+    public int dip2px(int dip) {
+        final float scale = getResources().getDisplayMetrics().density;
+        return (int) (dip * scale + 0.5f);
+    }
+
+    private void initDataSource() {
+        for (int i = 0; i < 10; i++) {
+            data.add(i + "");
+        }
+        adapter2.notifyDataSetChanged();
+    }
+}
